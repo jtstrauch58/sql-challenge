b26a93caa183e9442ff27f5fed79bd2aad638687
Jeremy Strauch <jtstrauch58@gmail.com>
2021-02-28 18:27:31 -0600

    first two questions answered 2.28 18.27

--- sqlchallenge_1.sql
+++ sqlchallenge_1.sql
@@ -13,7 +13,7 @@ create table employees (
 	hire_date date not null
 	);
 	
-select * from employees
+select * from employees;
 
 drop table departments;
 create table departments(
@@ -28,7 +28,9 @@ select * from emp_dept;
 
 create table emp_dept(
 	emp_no int not null,
-	dept_no varchar(50) not null
+	dept_no varchar(50) not null,
+	foreign key (emp_no) references employees(emp_no),
+	foreign key (dept_no) references departments(dept_no)
 	);
 select * from emp_dept;
 
@@ -52,6 +54,22 @@ create table titles(
 	title_id varchar(50) primary key,
 	title varchar(50) not null
 	);
+
+--List of employee number, last name, first name, sex, and salary
+create view employee_table as
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+from employees as e join salaries as s
+on e.emp_no = s.emp_no
+order by e.emp_no;
+select * from employee_table;
+
+-- List first name, last name, and hire date for employees who were hired in 1986
+create view table_1986_employees as
+select e.first_name, e.last_name, e.hire_date from employees as e
+where e.hire_date between '1986-01-01' and '1986-01-31'
+order by e.hire_date;
+select * from table_1986_employees;
+select count(hire_date) from table_1986_employees as num_hired_1986;
 
 
 
