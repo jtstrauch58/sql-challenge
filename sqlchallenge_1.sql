b033a1ca7ab87be5bf530ee139c3f034f28bf593
Jeremy Strauch <jtstrauch58@gmail.com>
2021-02-28 19:47:09 -0600

    all questions complete 2.28 19:47

--- sqlchallenge_1.sql
+++ sqlchallenge_1.sql
@@ -12,12 +12,13 @@ create table employees (
 	sex char(1) not null,
 	hire_date date not null
 	);
-
-select count(emp_no) from employees;
 select * from employees
 order by hire_date;
+select count(emp_no) from employees;
+
 
 drop table departments;
+
 create table departments(
 	dept_no varchar(50) primary key,
 	dept_name varchar(50) not null
@@ -70,7 +71,8 @@ create view table_1986_employees as
 select e.first_name, e.last_name, e.hire_date from employees as e
 where e.hire_date between '1986-01-01' and '1986-01-31'
 order by e.hire_date;
-select * from table_1986_employees;
+select * from table_1986_employees
+limit 10;
 select count(hire_date) from table_1986_employees as num_hired_1986;
 
 -- List the manager of each department with the following information: department number,
@@ -99,6 +101,42 @@ join table_managers_current as tm on tm.dept_no = d.dept_no
 join employees as e on tm.emp_no = e.emp_no
 order by tm.dept_no;
 select * from current_managers
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e join emp_dept on e.emp_no = emp_dept.emp_no
+join departments as d on emp_dept.dept_no = d.dept_no
+order by d.dept_name
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+create view hercules as
+select e.first_name, e.last_name, e.sex from employees as e
+where e.first_name = 'Hercules'
+and e.last_name like 'B%'
+order by e.last_name;
+select count(first_name) from hercules;
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e join emp_dept on e.emp_no = emp_dept.emp_no
+join departments as d on emp_dept.dept_no = d.dept_no
+where d.dept_name = 'Sales'
+order by e.last_name
+limit 10;
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e join emp_dept on e.emp_no = emp_dept.emp_no
+join departments as d on emp_dept.dept_no = d.dept_no
+where d.dept_name = 'Sales' or d.dept_name = 'Development'
+order by e.last_name
+limit 10;
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select last_name, count(last_name) as "count of shared names" from employees
+group by last_name
+order by "count of shared names" desc
+
 
 
 
